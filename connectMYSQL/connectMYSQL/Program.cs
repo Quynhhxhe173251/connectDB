593fc71c10e0104df9e4f35c5089a860c369b0d6
Quynhhxhe173251 <quynhhxhe173251@fpt.edu.vn>
2024-06-07 14:17:55 +0700

    up code connectDB

---
+++ connectMYSQL/connectMYSQL/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConnectMySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;user=root;database=DbProjectCSharp;port=3306;password=12345";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to MySQL...");
+                    conn.Open();
+
+                    // Tạo và thực thi câu lệnh SQL
+                    string sql = "SELECT * FROM user";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                    }
+                    rdr.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                    Console.WriteLine("Done.");
+                }
+            }
+        }
+    }
+
+}
+
